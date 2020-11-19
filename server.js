0b9af9f71f3b461250db446383f981b0a66dc48d
Ben <benjamin.sue@live.ca>
2020-11-18 19:50:51 -0800

    Secret change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use(flash());
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: 'temp_secret',
     resave: false,
     saveUninitialized: false
 }));
